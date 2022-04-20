bc3ff0d522122b77069c69e53263e714b21885d0
viggyram-geog <vramachandr9@wisc.edu>
2022-04-20 14:40:53 -0500

    updated lib, js, began createMap()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//insert code here!
+
+function createMap() {
+
+   var map = L.map('map').setView([30, -105], 1);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+}
+
+document.addEventListener('DOMContentLoaded', createMap);
