6b517251e7f2061ef9dadba3a9b1cef1e5690a1b
annanbeck <58147877+annanbeck@users.noreply.github.com>
2022-05-07 16:22:26 -0500

    changed position of zoom

--- js/mainab.js
+++ js/mainab.js
@@ -31,6 +31,28 @@ function createMap() {
         zoom: 4,
         layers: [OSM, Esri_WorldImagery, darkBasemap]
     });
+    //repositions the zoom controls so they are visible
+    function addControlPlaceholders(map) {
+        var corners = map._controlCorners,
+            l = 'leaflet-',
+            container = map._controlContainer;
+    
+        function createCorner(vSide, hSide) {
+            var className = l + vSide + ' ' + l + hSide;
+    
+            corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+        }
+    
+        createCorner('verticalcenter', 'left');
+        createCorner('verticalcenter', 'right');
+    }
+    addControlPlaceholders(map);
+    
+    // Change the position of the Zoom Control to a newly created placeholder.
+    map.zoomControl.setPosition('verticalcenterright');
+    
+    // You can also put other controls in the same placeholder.
+    L.control.scale({position: 'verticalcenterright'}).addTo(map);
 
     /*Legend specific*/
 
